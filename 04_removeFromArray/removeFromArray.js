382d373bc7f9eba293a55fc1c73bc4e07fc40ecb
Juan Marco <jmmarco@gmail.com>
2022-03-16 11:54:01 +0100

    Add solution for removeFromArray and enable tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...elements) {
+  return arr.reduce((acc, value) => {
+    if (!elements.includes(value)) {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
 };
 
 // Do not edit below this line
