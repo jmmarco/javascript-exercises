5a1c5828b89b69325d3044b7ce4d0725b9fc3e91
Juan Marco <jmmarco@gmail.com>
2022-03-16 14:47:40 +0100

    Add solution for calculator and enable tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((acc, number) => {
+    return acc + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acc, number) => {
+    return acc * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) return 1;
+  let result = 1;
+  for (let i = n; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
