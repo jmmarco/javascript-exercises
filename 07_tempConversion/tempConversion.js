0cf607f89a29fdd7accd4f2e90ebeed77b17e037
Juan Marco <jmmarco@gmail.com>
2022-03-16 14:04:59 +0100

    Add solution for tempConversion and enable tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  const cTemp = (fTemp - 32) / 1.8;
+  return cTemp === 0 ? cTemp : Number(cTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  const fTemp = cTemp * 1.8 + 32;
+  return fTemp === 0 ? fTemp : Number(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
