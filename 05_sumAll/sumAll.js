a49abbe11baa3673c40cde5ae0962fe7903baae8
Juan Marco <jmmarco@gmail.com>
2022-03-16 12:29:43 +0100

    Add solution for sumAll and enable tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let result = 0,
+    start = a,
+    end = b;
+
+  if (start > end) {
+    start = b;
+    end = a;
+  }
+
+  for (let n = start; n <= end; n++) {
+    result += n;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
