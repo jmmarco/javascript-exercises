59db3297a0285b02e8638c7ea3d510ccfa1e27f2
Juan Marco <jmmarco@gmail.com>
2022-03-26 11:18:42 +0100

    Add solution for palindromes and enable tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const original = str.toLowerCase().split(/\W+/gi).join("");
+  const reversed = reverseStr(str.toLowerCase()).split(/\W+/gi).join("");
+  return original === reversed;
 };
+
+function reverseStr(str) {
+  let resultStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    resultStr += str[i];
+  }
+  return resultStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
