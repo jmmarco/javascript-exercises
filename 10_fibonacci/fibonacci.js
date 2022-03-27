d02e3e3ffeb54082dc5fd3b7dd8519c0b79d099c
Juan Marco <jmmarco@gmail.com>
2022-03-27 17:21:54 +0200

    Add solution for fibonacci and enable all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  const sequence = [0, 1];
 
+  for (let i = 1; i < n; i++) {
+    sequence.push(
+      sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    );
+  }
+
+  return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
