2f4889d9a3457dd2655861a694ed5cdf184dfd8a
Juan Marco <jmmarco@gmail.com>
2022-03-27 17:23:09 +0200

    Add solution for findTheOldest and enable tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  function compareYears(a, b) {
+    const getYearOfDeath = (person) => {
+      return person.yearOfDeath || new Date().getFullYear();
+    };
 
+    return (
+      getYearOfDeath(a) - a.yearOfBirth - (getYearOfDeath(b) - b.yearOfBirth)
+    );
+  }
+
+  return arr.sort(compareYears)[arr.length - 1];
 };
 
 // Do not edit below this line
